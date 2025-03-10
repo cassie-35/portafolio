b2bcf492a6c93628f4461e74ada058ba2001eb74
Cassandra Damian <cndv35@gmail.com>
2025-03-09 20:10:57 -0500

    xfcgvh

--- script.js
+++ script.js
@@ -14,17 +14,15 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
-  document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.getElementById("menuToggle");
     const navMenu = document.getElementById("navMenu");
-    const menuLinks = document.querySelectorAll("#navMenu a"); // Selecciona todos los enlaces del menú
+    const menuLinks = document.querySelectorAll("#navMenu a"); 
 
-    // Abrir/Cerrar menú
     menuToggle.addEventListener("click", function () {
         navMenu.classList.toggle("active");
     });
 
-    // Cerrar menú al hacer clic en cualquier enlace
     menuLinks.forEach(link => {
         link.addEventListener("click", function () {
             navMenu.classList.remove("active");
